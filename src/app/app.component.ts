1b47bf0a9f5cc9776c2793277aaec02ace610e0d
Pradeep Singh <psingh10021998@gmail.com>
2023-12-27 16:06:24 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo_list';
+  constructor() {
+    //   setTimeout(() => {
+    //     this.title = "Change Title"
+    //   }, 2000);
+  }
 }
